aa68a8ee37f83e3cc3ed4d42794a1e52ea02939a
HE007PLAYS <41343831+HE007PLAYS@users.noreply.github.com>
2018-07-25 20:57:58 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //Requring the files
 const botconfig = require("./botconfig.json");
-const token = process.env.token;
+const tokenfile = require("./token.json");
 const Discord = require("discord.js");
 const fs = require("fs");
 const bot = new Discord.Client({disableEveryone: true});
@@ -84,4 +84,4 @@ setTimeout(() => {
 
 });
 
-bot.login(tokenfile.token);
+bot.login(process.env.token);
