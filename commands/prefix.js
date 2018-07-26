3a4c580f987300a5373644a86bcb37cb80fea636
HE007PLAYS <41343831+HE007PLAYS@users.noreply.github.com>
2018-07-26 17:08:40 +0100

    Add files via upload

--- commands/prefix.js
+++ commands/prefix.js
@@ -3,25 +3,25 @@ const fs = require("fs");
 
 module.exports.run = async (bot, message, args) => {
 
-if(!message.member.hasPermission("MANAGE_SERVER")) return message.reply("Ur to poor for this command me fam");
-if(!args[0] || args[0 == "help"]) return message.reply("Usage: lmaoprefix <desired prefix here>");
+  if(!message.member.hasPermission("MANAGE_SERVER")) return message.reply("No no no.");
+  if(!args[0] || args[0 == "help"]) return message.reply("Usage: !prefix <desired prefix here>");
 
-let prefixes = JSON.parse(fs.readFileSync("./prefixes.json", "utf-8"));
+  let prefixes = JSON.parse(fs.readFileSync("./prefixes.json", "utf8"));
 
-prefixes[message.guild.id] = {
-  prefixes: args[0]
-};
+  prefixes[message.guild.id] = {
+    prefixes: args[0]
+  };
 
-fs.writeFile("./prefixes.json", JSON.stringify(prefixes), (err) => {
-  if(err) console.log(err)
-});
+  fs.writeFile("./prefixes.json", JSON.stringify(prefixes), (err) => {
+    if (err) console.log(err)
+  });
 
-let sEmbed = new Discord.RichEmbed()
-.setColor("FF9900")
-.setTitle("Prefix set!")
-.setDescription(`Set to ${args[0]}`);
+  let sEmbed = new Discord.RichEmbed()
+  .setColor("#FF9900")
+  .setTitle("Prefix Set!")
+  .setDescription(`Set to ${args[0]}`);
 
-message.channel.send(sEmbed);
+  message.channel.send(sEmbed);
 
 }
 
